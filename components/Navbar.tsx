5eb3f648d6ed0f48a4d7d0ddadc89c472409ce2a
3bud-ZC <s-abdallah.ali@zewailcity.edu.eg>
2026-02-07 01:10:55 +0200

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,1 +1,12 @@
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <nav style={{ padding: "16px", borderBottom: "1px solid #eee" }}>
+      <Link to="/" style={{ marginRight: 12 }}>Home</Link>
+      <Link to="/scholarships" style={{ marginRight: 12 }}>Scholarships</Link>
+      <Link to="/login">Login</Link>
+    </nav>
+  );
+}
 
